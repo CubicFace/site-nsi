4f1bc7cac3801381b3dd0635bbb06fdfcd5c5b25
Gradient <thedjplays.gaming@gmail.com>
2020-05-31 18:07:41 +0200

    Ajout d'un thème sombre

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-new Vue({
+app=new Vue({
     el:'#main',
     vuetify: new Vuetify(),
     data:{
         darkMode:false,
         drawerEnabled:false,
         contactEnabled:false,
+        logIn:false,
+        signIn:false,
+    }
+})
+
+setInterval(function(){
+    if(app.darkMode){
+        document.getElementById("header").style.backgroundColor="#2e2d2d";
+        document.body.style.color="#ffffff";
+    }
+    else{
+        document.getElementById("header").style.backgroundColor="#f2f2f2";
+        document.body.style.color="#000000";
     }
 })
