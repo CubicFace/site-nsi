0ac0831bc6b3f02437dae4f87c6b686b03c4f691
Gradient <thedjplays.gaming@gmail.com>
2020-05-31 14:05:11 +0200

    Ajout d'un menu vertical pour une largeur petite

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@ new Vue({
     el:'#main',
     vuetify: new Vuetify(),
     data:{
-        drawerEnabled:false,
-        drawerItems:[
-            {title: 'Acceuil', icon:'mdi-home'},
-            {title: 'Classes', icon:'mdi-teach'},
-            {title: 'Contact', icon:'mdi-clipboard-user-outline'}
-        ]
+        drawerEnabled:false
     }
 })
