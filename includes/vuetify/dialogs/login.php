5f6e01fe9b7929a158553c746f88068bb6b5dcff
Gradient <thedjplays.gaming@gmail.com>
2020-06-08 23:01:13 +0200

    Vérification d'e-mail un peu plus poussée

--- includes/vuetify/dialogs/login.php
+++ includes/vuetify/dialogs/login.php
@@ -16,9 +16,12 @@
                                     </v-btn>
                                     <v-toolbar-title>Se connecter / S'inscrire</v-toolbar-title>
                                 </v-toolbar>
-                                <div class="d-flex flex-column my-6 align-center">
-                                    <v-card>
-                                        <v-card-title max-width="600px" class="elevation-12">Se connecter</v-card-title>
+                                <div class="d-flex flex-column my-6 align-center justify-space-around">
+                                    <v-card max-width="600px" class="mb-3">
+                                        <v-card-title class="elevation-12">Se connecter</v-card-title>
+                                    </v-card>
+                                    <v-card max-width="600px" class="mt-3">
+                                        <v-card-title class="elevation-12">S'inscrire</v-card-title>
                                     </v-card>
                                 </div>
                             </v-card>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 app=new Vue({
     el:'#main',
-    vuetify: new Vuetify(),
+    vuetify: new Vuetify(), 
     data:{
         darkMode:false,
         drawerEnabled:false,
@@ -12,7 +12,20 @@ app=new Vue({
         ],
         carouselPlaceholder:[
             "red","pink","blue","cyan","indigo","purple","indigo darken-3","blue darken-4","green","lime darken-2","yellow darken-4","amber darken-4"
-        ]
+        ],
+        formRules:{
+            email:[
+                v =>/[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\b){1,2}/.test(v)||'Format e-mail invalide.'
+            ],
+            password:[
+                v=>v.length >=5||'Le mot de passe doit contenir au moins 5 caractères'
+            ],
+            name:[
+                v=>v.length <=100||"Le nom est trop long",
+                v=>v.length >=5||"Le nom est trop court",
+                v=>/[A-Za-zÀ-ȕ0-9_ ]{5,100}/.test(v)||"Caractère invalide."
+            ]
+        }
     }
 })
 
