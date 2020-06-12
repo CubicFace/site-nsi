89da0e42f335c69aa013019b4dd7dd3906c9d542
Gradient <thedjplays.gaming@gmail.com>
2020-06-12 16:50:19 +0200

    Encore

--- classes.php
+++ classes.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
-    
-    <head>
-        
-        <title>NSI | Saint-Charles || Nos Classes</title>
-        <?php include "includes/head.html"?>
-    </head>
-    
+
+<head>
+
+    <title>NSI | Saint-Charles || Nos Classes</title>
+    <?php include "includes/head.html" ?>
+</head>
 
 
-    <body>
-        <div id="main">
-            <v-app >
 
-                <?php include "includes/header.html" ?>
+<body>
+    <div id="main">
+        <v-app>
+
+            <?php include "includes/header.html" ?>
 
             <!-- Il faut trouvé je pense des img pour illustrer un peu la page d'accueil -->
 
-                <?php include "includes/vuetify/nav-drawer.html" ?>
-                
-                <?php include "includes/vuetify/dialogs/contact.php" ?>
+            <?php include "includes/vuetify/nav-drawer.html" ?>
 
-                <?php include "includes/footer.html" ?>
-            </v-app>
-        </div>
+            <?php include "includes/vuetify/dialogs/contact.php" ?>
 
-    </body>
-    <?php include "includes/vuetify/js.html" ?>
-    <?php
-    if(isset($_GET['dark']) && $_GET['dark']==true){
-        echo "<script>app.darkMode=true</script>";
-    }
-    ?>
+            <?php include "includes/footer.html" ?>
+        </v-app>
+    </div>
+
+</body>
+<?php include "includes/vuetify/js.html" ?>
+<?php
+if (isset($_GET['dark']) && $_GET['dark'] == true) {
+    echo "<script>app.darkMode=true</script>";
+}
+?>
+
 </html>
--- index.php
+++ index.php
@@ -1,58 +1,47 @@
 <!DOCTYPE html>
 <html lang="fr">
-    
-    <head>
-        
-        <title>NSI | Saint-Charles</title>
-        <?php include "includes/head.html"?>
-    </head>
-    <body>
-        <div id="main">
-            <v-app >
 
-                <?php include "includes/header.html" ?>
+<head>
 
-                <v-carousel
-                cycle
-                height="640px"
-                show-arrows
-                show-arrows-on-hover
-                >
-                    <v-carousel-item
-                    v-for="item in carouselItems"
-                    :src="item.src"
-                    >
-                    </v-carousel-item>
-                    <v-carousel-item
-                    v-for="color in carouselPlaceholder"
-                    >
-                        <v-sheet
-                        :color="color"
-                        height="100%"
-                        >
-                            <div class="d-flex flex-column justify-space-around align-center">
-                                <div class="display-3">Insérer image</div>
-                                <div class="subtitle-1">{{color}}</div>
-                            </div>
-                        </v-sheet>
+    <title>NSI | Saint-Charles</title>
+    <?php include "includes/head.html" ?>
+</head>
 
-                    </v-carousel-item>
-                </v-carousel>
+<body>
+    <div id="main">
+        <v-app>
+
+            <?php include "includes/header.html" ?>
+
+            <v-carousel cycle height="640px" show-arrows show-arrows-on-hover>
+                <v-carousel-item v-for="item in carouselItems" :src="item.src">
+                </v-carousel-item>
+                <v-carousel-item v-for="color in carouselPlaceholder">
+                    <v-sheet :color="color" height="100%">
+                        <div class="d-flex flex-column justify-space-around align-center">
+                            <div class="display-3">Insérer image</div>
+                            <div class="subtitle-1">{{color}}</div>
+                        </div>
+                    </v-sheet>
+
+                </v-carousel-item>
+            </v-carousel>
             <!-- Il faut trouvé je pense des img pour illustrer un peu la page d'accueil -->
 
-                <?php include "includes/vuetify/nav-drawer.html" ?>
-                
-                <?php include "includes/vuetify/dialogs/contact.php" ?>
+            <?php include "includes/vuetify/nav-drawer.html" ?>
 
-                <?php include "includes/footer.html" ?>
-            </v-app>
-        </div>
+            <?php include "includes/vuetify/dialogs/contact.php" ?>
 
-    </body>
-    <?php include "includes/vuetify/js.html" ?>
-    <?php
-    if(isset($_GET['dark']) && $_GET['dark']==true){
-        echo "<script>app.darkMode=true</script>";
-    }
-    ?>
+            <?php include "includes/footer.html" ?>
+        </v-app>
+    </div>
+
+</body>
+<?php include "includes/vuetify/js.html" ?>
+<?php
+if (isset($_GET['dark']) && $_GET['dark'] == true) {
+    echo "<script>app.darkMode=true</script>";
+}
+?>
+
 </html>
