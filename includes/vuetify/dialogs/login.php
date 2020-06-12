342da5faca30b219f6d946893bcc0fcef4b92e50
Gradient <thedjplays.gaming@gmail.com>
2020-06-12 16:17:23 +0200

    Euh ouais presque

--- includes/vuetify/dialogs/login.php
+++ includes/vuetify/dialogs/login.php
@@ -19,20 +19,33 @@
                                 <div class="d-flex flex-column my-6 align-center justify-space-around">
                                     <v-card min-width="350px" max-width="400px" class="mb-3 elevation-12">
                                         <v-card-title>Se connecter</v-card-title>
-                                            <v-form ref="loginForm" v-model="formModels.loginValid" class="mx-5">
+                                            <v-form ref="loginForm" v-model="formModels.loginValid" class="mx-5 mb-5">
                                                 <v-text-field
                                                 v-model="formModels.loginEmail"
                                                 :rules="formRules.email"
+                                                :disabled="formModels.loginLoading"
                                                 label="E-mail"
                                                 type="email"
                                                 required
                                                 ></v-text-field>
                                                 <v-text-field
                                                 v-model="formModels.loginPassword"
+                                                :rules="formRules.loginPassword"
+                                                :disabled="formModels.loginLoading"
                                                 label="Mot de passe"
                                                 type="password"
                                                 required
                                                 ></v-text-field>
+                                                <v-card-actions>
+                                                    <v-spacer></v-spacer>
+                                                    <v-btn
+                                                    :disabled="!formModels.loginValid || formModels.loginLoading"
+                                                    depressed
+                                                    :loading="formModels.loginLoading"
+                                                    color="light-blue"
+                                                    @click="loginValidate"
+                                                    >Se connecter</v-btn>
+                                                </v-card-actions>
                                             </v-form>
                                     </v-card>
                                     <v-card max-width="360px" class="mt-3 elevation-12">
@@ -40,5 +53,5 @@
                                     </v-card>
                                 </div>
                             </v-card>
-
+                            <?php include "loginStatus.html" ?>
                         </v-dialog>
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ app=new Vue({
     el:'#main',
     vuetify: new Vuetify(), 
     data:{
+        test:'klnsdoksndklvsfn',
+        nonBool:false,
         darkMode:false,
         drawerEnabled:false,
         contactEnabled:false,
@@ -16,12 +18,17 @@ app=new Vue({
         
             formModels:{
                 loginValid:false,
+                loginLoading:false,
                 regValid:false,
+                regLoading:false,
                 loginEmail:'',
                 loginPassword:'',
                 regEmail:'',
                 regPassword:'',
-                regName:''
+                regName:'',
+                statusSheet:false,
+                sheetTitle:'',
+                sheetMsg:'',
             },
             formRules:{
                 email:[
@@ -34,19 +41,71 @@ app=new Vue({
                     v=>v.length <=100||"Le nom est trop long",
                     v=>v.length >=5||"Le nom est trop court",
                     v=>/[A-Za-zÀ-ȕ0-9_ ]{5,100}/.test(v)||"Caractère invalide."
+                ],
+                loginPassword:[
+                    v=>v.length>0||"Ce champ est requis."
                 ]
             }
         
     },
     methods:{
         regValidate:function(){},
-        loginValidate:function(){}
+        loginValidate:function(){
+            this.$data.formModels.loginLoading=true
+            loginData={
+                "action":"login",
+                "email":this.$data.formModels.loginEmail,
+                "password":this.$data.formModels.loginPassword
+            }
+            if(this.$refs.loginForm.validate()){
+                var request=new XMLHttpRequest();
+                request.open("POST","api/loginApi",true);
+                request.setRequestHeader("Content-Type","application/json");
+                request.onreadystatechange=function(){
+                    if(request.readyState===4){
+                        var response=JSON.parse(request.responseText);
+                        switch (request.status) {
+                            case 200:
+                                app.$data.formModels.sheetTitle="Vous êtes connecté!";
+                                app.$data.formModels.sheetMsg=`Bienvenue ${response.name}`;
+                                break;
+                            
+                            case 400:
+                                app.$data.formModels.sheetTitle="Erreur (400: Bad request)";
+                                app.$data.formModels.sheetMsg=`Erreur syntaxique de la requête. Message: '${response.status}'`;
+                                break;
+                            
+                            case 403:
+                                app.$data.formModels.sheetTitle="Erreur (403: Forbidden)";
+                                app.$data.formModels.sheetMsg="L'addresse e-mail ou le mot de passe entré est incorrect.";
+                                break;
+                            
+                            case 405:
+                                app.$data.formModels.sheetTitle="Erreur (405: Method Not Allowed)";
+                                app.$data.formModels.sheetMsg=`La méthode de la requête n'est pas acceptée. Message: '${response.status}'`;
+                                break;
+                            
+                            default:
+                                this.$data.formModels.sheetTitle=`Erreur (${request.status}: ${request.statusText})`
+                                this.$data.formModels.sheetMsg="Erreur inconnue.";
+                                
+                                break;
+                        };
+                        app.$data.formModels.loginLoading=false
+                        app.$data.formModels.statusSheet=true
+                    };
+                };
+                var data=JSON.stringify(loginData);
+                request.send(data);
+                
+            }
+        }
         
     }
 })
 
 setInterval(function(){
-    app.$vuetify.theme.dark=app.darkMode
+    app.$vuetify.theme.dark=app.darkMode;
 })
 
 setInterval(function(){
